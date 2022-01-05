e7f11028da3debb3247dabc4101d911b1dbbdf37
SameechaSud01 <96413079+SameechaSud01@users.noreply.github.com>
2022-01-05 12:16:21 +0530

    Add files via upload

---
+++ SEE.java
@@ -0,0 +1,19 @@
+package SEE;
+
+import CIE.*;
+
+import java.util.Scanner;
+
+
+public class externals extends CIE.student
+{
+ Scanner scan =  new Scanner(System.in);
+ public int seem[] = new  int [5];
+   
+
+ public void accept()
+{
+  for(int i = 0; i<5; i++)
+{
+   System.out.println("Enter SEE marks:");
+   
